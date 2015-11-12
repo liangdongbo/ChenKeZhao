111674c6e179644c1ae89966bf5a5f7d32e6a7c5
chenkezhao <310771881@qq.com>
2015-11-12 08:30:25 +0800

    动画-补间动画

--- app/src/main/java/com/ckz/thought/app/ReactionActivity.java
+++ app/src/main/java/com/ckz/thought/app/ReactionActivity.java
@@ -2,6 +2,11 @@ package com.ckz.thought.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ckz.thought.R;
 
@@ -17,6 +22,20 @@ public class ReactionActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reaction);
+
+
+        TextView tv = (TextView) findViewById(R.id.xx);
+
+        ImageView im = (ImageView) findViewById(R.id.ivxx);
+        //加载xml动画文件并将其设置到指定的View上
+
+        //透明
+        Animation alphaAnimation = AnimationUtils.loadAnimation(this,R.anim.tween_alpha);
+        tv.setAnimation(alphaAnimation);
+
+        //动画集合
+        AnimationSet animationSet = (AnimationSet) AnimationUtils.loadAnimation(this,R.anim.tween_animset);
+        im.setAnimation(animationSet);
     }
 
 }
