5db03385c26ac28628fc2dc2b221eb76a0f7f19e
chenkezhao <310771881@qq.com>
2015-11-12 08:39:02 +0800

    动画-帧动画

--- app/src/main/java/com/ckz/thought/app/ReactionActivity.java
+++ app/src/main/java/com/ckz/thought/app/ReactionActivity.java
@@ -1,5 +1,6 @@
 package com.ckz.thought.app;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -17,6 +18,7 @@ import com.ckz.thought.R;
  */
 public class ReactionActivity extends AppCompatActivity{
 
+    private ImageView im;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +28,7 @@ public class ReactionActivity extends AppCompatActivity{
 
         TextView tv = (TextView) findViewById(R.id.xx);
 
-        ImageView im = (ImageView) findViewById(R.id.ivxx);
+        im = (ImageView) findViewById(R.id.ivxx);
         //加载xml动画文件并将其设置到指定的View上
 
         //透明
@@ -34,8 +36,29 @@ public class ReactionActivity extends AppCompatActivity{
         tv.setAnimation(alphaAnimation);
 
         //动画集合
-        AnimationSet animationSet = (AnimationSet) AnimationUtils.loadAnimation(this,R.anim.tween_animset);
-        im.setAnimation(animationSet);
+        /*AnimationSet animationSet = (AnimationSet) AnimationUtils.loadAnimation(this,R.anim.tween_animset);
+        im.setAnimation(animationSet);*/
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //帧动画
+        //第一步将animation-list设置为ImageView的背景
+
+        im.setBackgroundResource(R.drawable.framelist);
+
+        //第二步获取ImagView的背景并将其转换成AnimationDrawable
+
+        /*提示：
+        有一点需要强调的是：启动Frame动画的代码animationDrawable.start();不能应用在OnCreate()方法中，
+        因为在OnCreate()中AnimationDrawable还没有完全的与ImageView绑定。在OnCreate()中启动动画，只能看到第一张图片*/
+        AnimationDrawable animationDrawable = (AnimationDrawable) im.getBackground();
+        //第三步开始播放动画
+
+        animationDrawable.start();
+    }
 }
